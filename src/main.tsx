a00b5201726fe1cc14ce3d2bd40bf5cb49b131d8
Moksh <moksh.rajput262632@gmail.com>
2026-09-21 09:43:48 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import logo from './assets/logo.png';
+
+// Dynamically favicon set karne ke liye
+const favicon = document.querySelector("link[rel='icon']") as HTMLLinkElement;
+if (favicon) {
+  favicon.href = logo;
+}
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
